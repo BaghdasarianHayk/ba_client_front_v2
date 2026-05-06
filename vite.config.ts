b0bcd18f9aaf0445bff202b16b62d0572a53c37c
Hayk Baghdasaryan <baghdasarianhayk@gmail.com>
2026-05-06 18:36:47 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/ba_client_front_v2/' : '/',
   plugins: [
     tanstackRouter({
       target: 'react',
